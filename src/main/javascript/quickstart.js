62f3c7b72620424dd3c1fe302b391ff288c2891f
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2018-08-28 23:22:27 +0530

    Hello world

---
+++ src/main/javascript/quickstart.js
@@ -0,0 +1,15 @@
+var DE_AKQUINET = DE_AKQUINET || {};
+DE_AKQUINET.quickstart = function () {
+
+return {
+        /**
+         * Adds two numbers ...
+         * @param number1 first number
+         * @param number2 second number
+         * @return the sum of the two numbers
+         */
+        addNumbers : function(number1, number2) {
+            return number1 + number2;
+        }
+    };
+} ();
