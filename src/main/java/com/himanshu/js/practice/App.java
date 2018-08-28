0980e8c504737db96729c88ac120463a4aff3d2a
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2018-08-28 22:31:51 +0530

    first commit

---
+++ src/main/java/com/himanshu/js/practice/App.java
@@ -0,0 +1,13 @@
+package com.himanshu.js.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
